0cf47b3312174c035bf837e8f0bf280bfe977c24
mainak <heellomeelo@gmail.com>
2023-09-05 16:11:45 +0530

    code to require dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/UserRoutes");
+require('dotenv').config();
 
 const app = express();
 
